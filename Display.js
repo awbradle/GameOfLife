bfceaec3caa83f218f124bd727278c28a257299a
Andrew Bradley <bradleyandrew13@gmail.com>
2015-11-16 20:28:25 -0500

    add onclick event

--- Display.js
+++ Display.js
@@ -1,15 +1,22 @@
-var game = document.getElementById("game");
-var gameLength = myLifeGame.cell.length;
+var myLifeGame = new lifeGame(40);
+var game = document.getElementById("game"); //gets the div with the Id "game"
+var gameLength = myLifeGame.cell.length; //
 //console.log(gameLength);
-//This loop initializes the game board
+//This loop initializes the game board on the screen
 for (var i = 0; i < gameLength; i++){
 	var row = document.createElement('div');
 	row.setAttribute('id', i);
 	
-	//row.innerHTML = i;
 	document.getElementById("game").appendChild(row);
 	for(var j = 0; j < gameLength; j++){
-		var cell = document.createElement('div');
+		cell = document.createElement('div');
+		this.cell.i = i;
+		this.cell.j = j;
+				if (myLifeGame.cell[i][j].alive)
+			cell.style.backgroundColor = "green";
+		else
+			cell.style.backgroundColor = "white";
+
 		cell.style.width = "10px";
 		cell.style.height = "10px";
 		cell.style.cssFloat = "left";
@@ -22,27 +29,43 @@ for (var i = 0; i < gameLength; i++){
 		cell.setAttribute('id', cellName);
 		if (j == 0)
 			cell.style.clear = "left";
-//		cell.addEventListener("click", myScript); change on click
-		document.getElementById(i).appendChild(cell);
 		
-	}
+		document.getElementById(i).appendChild(cell);
+		this.cell.onclick = clickThisCell;
+		}
+		
 }
+
+//This updates the cells' appearance on the screen. Recently alive cells appear red for one
+//turn, living cells are green, the rest of the non-living cells are white
 function updateCells (){
 	for (var i = 0; i < gameLength; i++){
 		for (var j = 0; j < gameLength; j++){
+			var cellName = "r" + i + "c" + j;
 			if (myLifeGame.cell[i][j].alive)
-				document.getElementById(("r"+i+"c"+ j)).style.backgroundColor = "green";
-			else if (document.getElementById(("r"+i+"c"+ j)).style.backgroundColor == "green")
-				document.getElementById(("r"+i+"c"+ j)).style.backgroundColor = "red";
+				document.getElementById(cellName).style.backgroundColor = "green";
+			else if (document.getElementById(cellName).style.backgroundColor == "green")
+				document.getElementById(cellName).style.backgroundColor = "red";
 			else
-				document.getElementById(("r"+i+"c"+ j)).style.backgroundColor = "white";
+				document.getElementById(cellName).style.backgroundColor = "white";
 				
 		}
 	}
 }
-
+//has the game perform a new turn
 function playGame(){
 	myLifeGame.setNextTurn();
 	myLifeGame.doTurn();
+	updateCells();
+}
+//This is the onclick event for clicking on a cell. Sets a living cell to dead and a dead
+//cell to alive. 
+function clickThisCell(){
+	
+	var i = this.i, j = this.j;
+	if (myLifeGame.cell[i][j].alive)
+		myLifeGame.setDead(i,j);
+	else
+		myLifeGame.setAlive(i,j);
 	updateCells();
 }
--- GameLife.js
+++ GameLife.js
@@ -6,7 +6,8 @@ var lifeUnit = function(){
 	this.neighbors = 0;
 
 }
-//this creates a game object made up of individual cells
+//this creates a game object made up of individual cells. Takes as an attribute the desired
+//number of cells to create an x by x gameboard
 var lifeGame = function(x){
 	this.x = x;
 	this.cell = [];
@@ -16,13 +17,16 @@ var lifeGame = function(x){
 			this.cell[i][j] = new lifeUnit();
 		}
 	}
+	//bring cell to life
 	this.setAlive = function(x,y){
 		this.cell[x][y].alive = true;
 	}
+	//kill a cell
 	this.setDead = function(x,y){
 		this.cell[x][y].alive = false;
 	}
-	//
+	//This function goes through each cell and sets the number of alive neighboring cells
+	//in the cells neighbor variable
 	this.setNextTurn = function(){
 		var rowLength = this.cell.length - 1;
 		for (var i = 1; i < rowLength; i++){
@@ -50,49 +54,19 @@ var lifeGame = function(x){
 		this.cell[rowLength][rowLength].neighbors = this.cell[rowLength][rowLength-1].alive + this.cell[rowLength-1][rowLength-1].alive +
 				this.cell[rowLength-1][rowLength].alive;  
 	}
-	
+	//This function takes the information in neighbors and uses it to set each cell to alive
+	//or dead based on Conway's Game of Life Rules
 	this.doTurn = function(){
 		var rowLength = this.cell.length;
 		for (var i = 0; i < rowLength; i++){
 			for (var j = 0; j < rowLength; j++){
-				if (this.cell[i][j].neighbors >= 4 || this.cell[i][j].neighbors < 2)
+				if (this.cell[i][j].neighbors >= 4 || this.cell[i][j].neighbors < 2) //overpopulation and underpopulation
 					this.setDead(i,j);
-				if (this.cell[i][j].neighbors == 3)
+				if (this.cell[i][j].neighbors == 3) //come alive
 					this.setAlive(i,j);
 			}
 		}
 	}
-// 	this.toConsole =  function(){
-// 		var rowLength = this.cell.length;
-// 		for (var i = 0; i < rowLength; i++){
-// 			var str = "";
-// 			for (var j = 0; j < rowLength; j++){ 
-// 				if (this.cell[i][j].alive)
-// 					str += "1";
-// 				else
-// 					str += "0";
-// 			}
-// 			console.log(str);
-// 		}
-// 	}
+
 }
-	
-
-
-// arr[1][2].alive = true;
-// console.log(arr[0][0].alive);
-// //console.log(arr[1][2].alive);
-// console.log(arr[-1] == undefined);
-
-var myLifeGame = new lifeGame(50);
-//myLifeGame.setAlive(4,3);
-//myLifeGame.setAlive(5,4);
-//myLifeGame.setAlive(0,0);
-//myLifeGame.setAlive(49,3);
-//console.log(myLifeGame.cell[4][3]);
-//myLifeGame.setNextTurn();
-//console.log(myLifeGame.cell[5][3]);
-//console.log(myLifeGame.cell[49][3]);
-//console.log(myLifeGame.cell[49][2]);
-
 
