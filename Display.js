39e495aa520e2f2c59b34a0558926255d86e9c81
Andrew Bradley <bradleyandrew13@gmail.com>
2015-11-17 10:47:05 -0500

    Final Version with code cleaned up

--- Display.js
+++ Display.js
@@ -1,15 +1,20 @@
-var timers = [],
-	gameSpeed = 300,
-	livingColor = "green";
-var myLifeGame = new lifeGame(40);
+//Andrew Bradley COSC 221
+//Fall 2015 Instructor: Gurnee
+//
+//This runs the graphics for the game of life
+//elements of the timer were borrowed from in class lesson
+
+var timers = [], //timer array
+	gameSpeed = 300, //initial game speed
+	livingColor = "green"; //initial life alive color
+var myLifeGame = new lifeGame(40); //create new game 40x40 cells
 var game = document.getElementById("game"); //gets the div with the Id "game"
-var gameLength = myLifeGame.cell.length; //
-//console.log(gameLength);
+var gameLength = myLifeGame.cell.length;
+
 //This loop initializes the game board on the screen
 for (var i = 0; i < gameLength; i++){
 	var row = document.createElement('div');
 	row.setAttribute('id', i);
-	
 	document.getElementById("game").appendChild(row);
 	for(var j = 0; j < gameLength; j++){
 		cell = document.createElement('div');
@@ -19,7 +24,7 @@ for (var i = 0; i < gameLength; i++){
 			cell.style.backgroundColor = livingColor;
 		else
 			cell.style.backgroundColor = "white";
-
+		//set cell style
 		cell.style.width = "10px";
 		cell.style.height = "10px";
 		cell.style.cssFloat = "left";
@@ -34,7 +39,7 @@ for (var i = 0; i < gameLength; i++){
 			cell.style.clear = "left";
 		
 		document.getElementById(i).appendChild(cell);
-		this.cell.onclick = clickThisCell;
+		this.cell.onclick = clickThisCell; //add onclick event
 		}
 		
 }
@@ -50,21 +55,21 @@ function updateCells (){
 			else if (document.getElementById(cellName).style.backgroundColor == livingColor)
 				document.getElementById(cellName).style.backgroundColor = "red";
 			else
-				document.getElementById(cellName).style.backgroundColor = "white";
-				
+				document.getElementById(cellName).style.backgroundColor = "white";		
 		}
 	}
 }
+
 //has the game perform a new turn
 function playGame(){
 	myLifeGame.setNextTurn();
 	myLifeGame.doTurn();
 	updateCells();
 }
+
 //This is the onclick event for clicking on a cell. Sets a living cell to dead and a dead
 //cell to alive. 
-function clickThisCell(){
-	
+function clickThisCell(){	
 	var i = this.i, j = this.j;
 	if (myLifeGame.cell[i][j].alive)
 		myLifeGame.setDead(i,j);
@@ -73,15 +78,20 @@ function clickThisCell(){
 	updateCells();
 }
 
+//pushes a timer into the timer array to run game
 function startGame() {
 	if (timers.length <= 0)
 		timers.push(setInterval(playGame, gameSpeed));
 }
+
+//stops the game from running
 function stopGame() {
 	while(timers.length > 0){
 		clearInterval(timers.pop());
 	}
 }
+
+//makes the game run faster
 function speedUp(){
 	if (gameSpeed > 100)
 	gameSpeed -= 100;
@@ -89,8 +99,9 @@ function speedUp(){
 		stopGame();
 		startGame();
 	}
-	
 }
+
+//makes the game run slower
 function slowDown(){
 	gameSpeed +=100
 	if (timers.length > 0){
@@ -98,15 +109,18 @@ function slowDown(){
 		startGame();
 	}
 }
+
+//change living cell color
 function changeColor(color){
 	livingColor = color;
 	updateCells();
 }
+
+//randomly generate some cells on the board
 function randomLife(){
-	
 	for (var i = 0; i < gameLength; i++){
 		for(var j = 0; j < gameLength; j++){
-			var num = (Math.floor(Math.random() * 2));
+			var num = (Math.floor(Math.random() * 1.4)); //how many random cells are generated
 			if(num)
 				myLifeGame.setAlive(i,j);
 			else
--- GameLife.js
+++ GameLife.js
@@ -1,3 +1,9 @@
+//Andrew Bradley COSC 221
+//Fall 2015 Instructor: Gurnee
+//
+//This runs the game of life. The game is a 2d array of lifeUnit objects
+//
+
 //the individual cell unit of the life game.
 //contains its status on whether it is alive and whether it is alive the next turn.
 //neighbors is how many surrounding cells are alive.
@@ -17,16 +23,19 @@ var lifeGame = function(x){
 			this.cell[i][j] = new lifeUnit();
 		}
 	}
+	
 	//bring cell to life
 	this.setAlive = function(x,y){
 		this.cell[x][y].alive = true;
 	}
+	
 	//kill a cell
 	this.setDead = function(x,y){
 		this.cell[x][y].alive = false;
 	}
+	
 	//This function goes through each cell and sets the number of alive neighboring cells
-	//in the cells neighbor variable
+	//in the cells neighbor variable. Current setup has borders acting as boundaries.
 	this.setNextTurn = function(){
 		var rowLength = this.cell.length - 1;
 		for (var i = 1; i < rowLength; i++){
@@ -54,6 +63,7 @@ var lifeGame = function(x){
 		this.cell[rowLength][rowLength].neighbors = this.cell[rowLength][rowLength-1].alive + this.cell[rowLength-1][rowLength-1].alive +
 				this.cell[rowLength-1][rowLength].alive;  
 	}
+	
 	//This function takes the information in neighbors and uses it to set each cell to alive
 	//or dead based on Conway's Game of Life Rules
 	this.doTurn = function(){
@@ -67,6 +77,4 @@ var lifeGame = function(x){
 			}
 		}
 	}
-
 }
-
